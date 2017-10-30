6316d29db17016c4f9340fdbea80031560a0698b
Imran Khan <m_imran_khan@me.com>
2017-10-30 18:00:06 +0100

    some styling

--- designs.js
+++ designs.js
@@ -2,32 +2,32 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-let row = '';
-let col = '';
+let row;
+let col;
 
 function makeGrid() {
 /* capture pixel_canvas ID */
-const table = document.getElementById('pixel_canvas');
-/* assign row to input_height */
-  row = document.getElementById('input_height').value;
-/* assign col to input_width */
-  col = document.getElementById('input_width').value;
-/* create row and col */
+const table = document.getElementById("pixel_canvas");
+  /* assign row to input_height */
+  row = document.getElementById("input_height").value;
+  /* assign col to input_width */
+  col = document.getElementById("input_width").value;
+  /* create row and col */
   for (let r = 0; r < row; r++) {
     const elt = table.insertRow(r);
-    elt.setAttribute('class', 'rows');
+    elt.setAttribute("class", "rows");
     for (let c = 0; c < col; c++) {
       const cell = elt.insertCell(c);
       /* add color to selected cell */
-      cell.addEventListener('click', function(evt){
-        evt.target.style.backgroundColor = document.getElementById('colorPicker').value;
+      cell.addEventListener("click", function(evt){
+        evt.target.style.backgroundColor = document.getElementById("colorPicker").value;
       });
     }
   }
   return false;
 }
 // Event listener on Submit button
-document.getElementById('sizePicker').addEventListener('submit', function(evt) {
+document.getElementById("sizePicker").addEventListener("submit", function(evt) {
   evt.preventDefault();
   makeGrid();
 });
